70f042e9355127f9025a8a5ed4fd1604ab3435fe
PatJ <67933612+patj@users.noreply.github.com>
2022-05-06 20:04:15 +0900

    アプリケーションが切り替わったときに、window[0]の中心にマウスカーソルを移動

--- mcm/AppDelegate.swift
+++ mcm/AppDelegate.swift
@@ -14,14 +14,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "quit", action: #selector(quit), keyEquivalent: "q"))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
         statusItem.menu = menu
         
         statusItem.button?.title = "MCM"
+        
+        
+        NSWorkspace.shared.notificationCenter.addObserver(self,
+                                                          selector: #selector(appChanged(_:)),
+                                                          name: NSWorkspace.didActivateApplicationNotification,
+                                                          object: nil)
+
     }
+
+    @objc func appChanged(_ notification: NSNotification) {
+        guard let pid: Int32 = NSWorkspace.shared.frontmostApplication?.processIdentifier else {
+            return
+        }
+
+        let targetWindow: NSDictionary? = getWindowList( pid:pid )?[0]
+
+        if( targetWindow != nil ){
+
+            let Height: Double? = ( ( targetWindow?[ kCGWindowBounds ] as? NSDictionary)?["Height"] ) as? Double
+            let Width: Double? = ( ( targetWindow?[ kCGWindowBounds ] as? NSDictionary)?["Width"] ) as? Double
+            let x: Double? = ( ( targetWindow?[ kCGWindowBounds ] as? NSDictionary)?["X"] ) as? Double
+            let y: Double? = ( ( targetWindow?[ kCGWindowBounds ] as? NSDictionary)?["Y"] ) as? Double
+
+            let pointX: Double = ( x ?? 0 ) + ( ( Width ?? 0 ) / 2 )
+            let pointY: Double = ( y ?? 0 ) + ( ( Height ?? 0 ) / 2 )
+            CGWarpMouseCursorPosition( CGPoint( x: pointX, y: pointY ) )
+        }
+    }
+
+    func getWindowList( pid:Int32 ) -> [NSDictionary]? {
+        guard let windowList: NSArray = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) else {
+            return nil
+        }
+
+        let tmpWindowList = windowList as! [NSDictionary]
+
+        let appWindowList = tmpWindowList.filter { (windowInfo: NSDictionary) -> Bool in
+            return windowInfo[kCGWindowOwnerPID] as! Int == pid
+        }
+
+        return appWindowList
+    }
+
+    
     
     @objc func quit(_ sender: NSMenuItem) {
-        print("quit")
         NSApplication.shared.terminate(self)
     }
 
