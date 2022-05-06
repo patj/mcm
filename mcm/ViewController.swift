47a3baed230ddd3a5b693e94a47d3b8f7fe459a6
PatJ <67933612+patj@users.noreply.github.com>
2022-05-06 18:00:18 +0900

    Initial Commit

---
+++ mcm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mcm
+//
+//  Created by tommy on 2022/05/06.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
